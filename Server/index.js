4445e46fa7cbaed88cf73f359d2779b5b57552f3
Sahil <garg07825@gmail.com>
2023-12-27 04:59:57 +0530

    created a class for construct the error

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { config } from "dotenv";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import { errorMiddleware } from "./middlewares/error.js";
 
 // here i am exporting app to server.js file
 export const app = express();
@@ -14,3 +15,6 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+
+// error handler middleware
+app.use(errorMiddleware);
