46bd3a032e00c452886b1b233a88506d63481e3b
Sahil <garg07825@gmail.com>
2023-12-27 13:01:09 +0530

    logout also added

--- Client/src/Survey/SurveyPage.jsx
+++ Client/src/Survey/SurveyPage.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 
 const SurveyPage = () => {
   const [surveys, setSurveys] = useState([]);
-  const { isAuthenticated } = useContext(AdminContext);
+  const { isAuthenticated, logoutUser } = useContext(AdminContext);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -22,15 +22,34 @@ const SurveyPage = () => {
     fetchSurveys();
   }, []);
 
+  const handleLogout = async () => {
+    try {
+      await axios.get("/admin/logout");
+      navigate("/login");
+      logoutUser();
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
+  };
+
+  // Redirect logic based on isAuthenticated state
   useEffect(() => {
-    if (isAuthenticated === false) {
+    if (!isAuthenticated) {
       navigate("/login");
     }
   }, [isAuthenticated, navigate]);
 
   return (
-    <div className="bg-black text-white min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-4xl mb-8">All Survey Forms</h1>
+    <div className="bg-black text-white min-h-screen flex flex-col">
+      <div className="flex justify-end p-4">
+        <button
+          onClick={handleLogout}
+          className="bg-red-600 text-white px-4 py-2 rounded-md hover:bg-red-700"
+        >
+          Logout
+        </button>
+      </div>
+      <h1 className="text-4xl mb-8 text-center">All Survey Forms</h1>
       <div className="container mx-auto grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {surveys.map((survey) => (
           <SurveyForm key={survey._id} survey={survey} />
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AdminProvider } from "./context/AdminContext.jsx";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AdminProvider>
