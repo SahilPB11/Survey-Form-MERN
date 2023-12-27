e3139a5f7e26f484b075e9607141a9c4b97f4848
Sahil <garg07825@gmail.com>
2023-12-27 12:30:34 +0530

    Login page is ready

--- Client/src/Admin/AdminLoginRegister.jsx
+++ Client/src/Admin/AdminLoginRegister.jsx
@@ -24,41 +24,38 @@ const AdminLoginRegister = () => {
   };
 
   return (
-    <div
-      className="min-h-screen flex items-center justify-center"
-      style={{ backgroundColor: "rgb(196, 186, 174)" }}
-    >
-      <div className=" p-8 rounded-lg shadow-md w-full max-w-screen-lg flex flex-col lg:flex-row" style={{ backgroundColor: "rgb(196, 186, 174)" }}>
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <div className="p-12 rounded-lg shadow-md w-full max-w-screen-2xl flex flex-col lg:flex-row bg-gray-800">
         {/* Form Section */}
-        <div className="flex-1 flex flex-col justify-center px-8 py-12">
-          <h2 className="text-3xl mb-6 text-center lg:text-left">
+        <div className="flex-1 flex flex-col justify-center px-12 py-16">
+          <h2 className="text-5xl mb-10 text-white text-center lg:text-left">
             {isLoginOrRegister === "register" ? "Register" : "Login"}
           </h2>
-          <form onSubmit={handleSubmit} className="space-y-4">
+          <form onSubmit={handleSubmit} className="space-y-6">
             <input
               type="text"
               placeholder="Username"
               value={userName}
               onChange={(e) => setUserName(e.target.value)}
-              className="w-full p-3 mb-4 border rounded-md focus:outline-none focus:border-blue-500"
+              className="w-full p-4 mb-6 border rounded-md focus:outline-none focus:border-blue-500 bg-gray-700 text-white"
             />
             <input
               type="password"
               placeholder="Password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="w-full p-3 mb-4 border rounded-md focus:outline-none focus:border-blue-500"
+              className="w-full p-4 mb-6 border rounded-md focus:outline-none focus:border-blue-500 bg-gray-700 text-white"
             />
             <button
               type="submit"
-              className="w-full bg-blue-500 text-white p-3 rounded-md hover:bg-blue-600"
+              className="w-full bg-blue-500 text-white p-4 rounded-md hover:bg-blue-600"
             >
               {isLoginOrRegister === "register" ? "Register" : "Login"}
             </button>
           </form>
-          <div className="text-center mt-4">
+          <div className="text-center mt-6">
             {isLoginOrRegister === "register" ? (
-              <p>
+              <p className="text-white">
                 Already have an account?{" "}
                 <button
                   onClick={() => setIsLoginOrRegister("login")}
@@ -68,7 +65,7 @@ const AdminLoginRegister = () => {
                 </button>
               </p>
             ) : (
-              <p>
+              <p className="text-white">
                 Don't have an account?{" "}
                 <button
                   onClick={() => setIsLoginOrRegister("register")}
