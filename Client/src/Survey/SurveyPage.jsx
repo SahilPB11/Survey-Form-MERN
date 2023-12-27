22965c74e59c2ec422cdf24472f6dac24ad75850
Sahil <garg07825@gmail.com>
2023-12-27 12:39:31 +0530

    project completed

--- Client/src/Survey/SurveyPage.jsx
+++ Client/src/Survey/SurveyPage.jsx
@@ -1,17 +1,18 @@
-// SurveyPage.jsx
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
-import SurveyForm from "../component/SurveyForm.jsx"; // Adjust the path based on your folder structure
+import SurveyForm from "../component/SurveyForm.jsx";
 import { AdminContext } from "../context/AdminContext.jsx";
 import { useNavigate } from "react-router-dom";
+
 const SurveyPage = () => {
   const [surveys, setSurveys] = useState([]);
   const { isAuthenticated } = useContext(AdminContext);
   const navigate = useNavigate();
+
   useEffect(() => {
     const fetchSurveys = async () => {
       try {
-        const response = await axios.get("/admin/forms"); // Adjust the API endpoint URL
+        const response = await axios.get("/admin/forms");
         setSurveys(response.data);
       } catch (error) {
         console.error("Error fetching surveys:", error);
@@ -23,16 +24,18 @@ const SurveyPage = () => {
 
   useEffect(() => {
     if (isAuthenticated === false) {
-      navigate("/login"); // Navigate to the login page
+      navigate("/login");
     }
   }, [isAuthenticated, navigate]);
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl mb-4">All Survey Forms</h1>
-      {surveys.map((survey) => (
-        <SurveyForm key={survey._id} survey={survey} />
-      ))}
+    <div className="bg-black text-white min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl mb-8">All Survey Forms</h1>
+      <div className="container mx-auto grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {surveys.map((survey) => (
+          <SurveyForm key={survey._id} survey={survey} />
+        ))}
+      </div>
     </div>
   );
 };
--- Client/src/component/SurveyForm.jsx
+++ Client/src/component/SurveyForm.jsx
@@ -1,29 +1,30 @@
-// components/SurveyForm.jsx
 import React from "react";
 
 const SurveyForm = ({ survey }) => {
   return (
-    <div className="bg-white p-6 rounded-lg shadow-md mb-4">
-      <h2 className="text-xl mb-2">Survey Form by {survey.name}</h2>
-      <p>
+    <div className="bg-gray-800 text-white p-8 rounded-lg shadow-lg mb-6 transition-all hover:scale-110 hover:bg-gray-700">
+      <h2 className="text-2xl mb-6 text-center font-bold">
+        Survey Form by {survey.name}
+      </h2>
+      <p className="text-lg mb-2">
         <strong>Gender:</strong> {survey.gender}
       </p>
-      <p>
+      <p className="text-lg mb-2">
         <strong>Nationality:</strong> {survey.nationality}
       </p>
-      <p>
+      <p className="text-lg mb-2">
         <strong>Email:</strong> {survey.email}
       </p>
-      <p>
+      <p className="text-lg mb-2">
         <strong>Phone Number:</strong> {survey.phoneNumber}
       </p>
-      <p>
+      <p className="text-lg mb-2">
         <strong>Address:</strong> {survey.address}
       </p>
-      <p>
+      <p className="text-lg mb-2">
         <strong>Message:</strong> {survey.message}
       </p>
-      <p>
+      <p className="text-lg mb-2">
         <strong>Created At:</strong>{" "}
         {new Date(survey.createdAt).toLocaleDateString()}
       </p>
